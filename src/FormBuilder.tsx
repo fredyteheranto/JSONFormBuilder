010c82e51a9424ef888e1a7f9a4efdd6d54c41bc
Alexander Weigelt <alexander.weigelt@ecube.de>
2023-04-07 10:20:57 +0200

    Render form fields dynamically

--- src/FormBuilder.tsx
+++ src/FormBuilder.tsx
@@ -1,25 +1,34 @@
-import React, { type FC, type ReactElement } from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik'
+import React, { createElement, type FC, type FormHTMLAttributes, type ReactNode } from 'react'
+import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import { type NumberSchema, type StringSchema } from 'yup'
 import Button from './Button'
+import InputText from './InputText'
+import Select from './Select'
 
 interface FormField {
-  name: string
+  component: string
+  id: string
   label: string
-  type: string
-  value?: string | number
+  className?: string
   validation?: StringSchema | NumberSchema
 }
 
-interface Props {
+type Props = {
   fields: FormField[]
   submitForm: (values: any) => void
+  buttonText?: string
+} & FormHTMLAttributes<HTMLFormElement>
+
+const Components: Record<string, any> = {
+  input: InputText,
+  select: Select,
+  button: Button
 }
 
-const FormBuilder: FC<Props> = ({ fields, submitForm }) => {
+const FormBuilder: FC<Props> = ({ fields, submitForm, buttonText = 'Submit', className, ...props }) => {
   const initialValues = fields.reduce(
-    (valuesObj, field) => ({ ...valuesObj, [field.name]: '' }),
+    (valuesObj, field) => ({ ...valuesObj, [field.id]: '' }),
     {}
   )
 
@@ -27,59 +36,49 @@ const FormBuilder: FC<Props> = ({ fields, submitForm }) => {
     fields.reduce(
       (schemaObj, field) => ({
         ...schemaObj,
-        [field.name]: field.validation ?? Yup.string()
+        [field.id]: field.validation ?? Yup.string()
       }),
       {}
     )
   )
 
-  const onSubmit = (values: any): void => {
-    submitForm(values)
-  }
+  const { handleChange, handleSubmit, isSubmitting } =
+    useFormik({
+      initialValues,
+      validationSchema,
+      onSubmit: async (values, { setSubmitting, resetForm }) => {
+        submitForm(values)
+        resetForm({ values: initialValues })
+        setSubmitting(false)
+      }
+    })
 
-  const renderField = (field: FormField): ReactElement | null => {
-    switch (field.type) {
-      case 'text':
-        return (
-          <div key={field.name}>
-            <label>{field.label}</label>
-            <Field
-              type="text"
-              name={field.name}
-            />
-            <ErrorMessage name={field.name} />
-          </div>
-        )
-      case 'number':
-        return (
-          <div key={field.name}>
-            <label>{field.label}</label>
-            <Field
-              type="number"
-              name={field.name}
-            />
-            <ErrorMessage name={field.name} />
-          </div>
-        )
-      // Add additional field types as needed
-      default:
-        return null
+  const renderField = (field: FormField): ReactNode => {
+    const { component, validation, className, ...props } = field
+    if (typeof Components[component] !== 'undefined') {
+      return createElement(
+        Components[component],
+        {
+          key: field.id,
+          onChange: handleChange,
+          ...props
+        }
+        // renderChildren(field.children)
+      )
     }
+    return createElement(
+      () => <div>It seems to be a mismatch in your form configuration schema on ID {field.id}.</div>,
+      { key: field.id }
+    )
   }
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={onSubmit}
-      validationSchema={validationSchema}
-    >
-      {() => (
-        <Form>
-          {fields.map((field) => renderField(field))}
-          <Button type="submit">Submit</Button>
-        </Form>
-      )}
-    </Formik>
+    <form onSubmit={handleSubmit} className={className}>
+      {fields.map((field, index) => <div key={index} className={field.className}>{renderField(field)}</div>)}
+      <Button type={'submit'} variant={'primary'} disabled={isSubmitting}>
+        {buttonText}
+      </Button>
+    </form>
   )
 }
 
